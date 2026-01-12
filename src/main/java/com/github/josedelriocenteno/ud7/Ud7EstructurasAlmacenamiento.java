6b3229dc62d43f270797e80ca597411d160585e9
josedelriocenteno <josemiguel.delrio.centeno@gregoriofer.com>
2026-01-12 17:04:21 +0100

    Inicial commit Ud7 Estructuras de Almacenamiento

---
+++ src/main/java/com/github/josedelriocenteno/ud7/Ud7EstructurasAlmacenamiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.josedelriocenteno.ud7;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class Ud7EstructurasAlmacenamiento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
