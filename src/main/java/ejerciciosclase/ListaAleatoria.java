ff0ece7054b79d30a56f67dfa15ee251960c5388
josemigueldelrio <josemiguel.delrio.centeno@gregoriofer.com>
2026-02-20 10:49:17 +0100

    array

---
+++ src/main/java/ejerciciosclase/ListaAleatoria.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosclase;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class ListaAleatoria {
+    
+    private static int[] array = new int[10];
+    private static Random rd = new Random();
+    
+    private static void crearImprimirArray(){
+        
+    }
+    
+    private static void imprimirArray(int[] array){
+        System.out.println(Arrays.toString(array));
+    }
+}
