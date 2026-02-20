4fee4632312934a46125b4b82e831d67510636b4
josemigueldelrio <josemiguel.delrio.centeno@gregoriofer.com>
2026-02-20 10:49:23 +0100

    array

---
+++ src/main/java/ejerciciosclase/Frases.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosclase;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class Frases {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
+    private static void imprimeFrases(String[] array){
+        System.out.println(array);
+    }
+    
+    
+    
+}
